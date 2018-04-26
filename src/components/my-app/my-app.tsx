4ba441ad7fe3ad8bb9242d97e74285b920e31932
Tim <tja2353@gmail.com>
2018-04-26 16:00:50 +0100

    (package.json) @stencil/core 0.7.27-2 @stencil/router 0.1.0

--- src/components/my-app/my-app.tsx
+++ src/components/my-app/my-app.tsx
@@ -1,12 +1,11 @@
 import { Component } from '@stencil/core';
-
+import { RouterSwitch } from '@stencil/router';
 
 @Component({
   tag: 'my-app',
-  styleUrl: 'my-app.css'
+  styleUrl: 'my-app.css',
 })
 export class MyApp {
-
   render() {
     return (
       <div>
@@ -16,11 +15,10 @@ export class MyApp {
 
         <main>
           <stencil-router>
-            <stencil-route url='/' component='app-home' exact={true}>
-            </stencil-route>
-
-            <stencil-route url='/profile/:name' component='app-profile'>
-            </stencil-route>
+            <RouterSwitch>
+              <stencil-route url="/" component="app-home" exact={true} />
+              <stencil-route url="/profile/:name" component="app-profile" />
+            </RouterSwitch>
           </stencil-router>
         </main>
       </div>
